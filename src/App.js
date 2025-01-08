2d061ecaafbd79749d1c7bb65a6840e769abb0e9
pmarathay <pmarathay@infiniteoptions.com>
2025-01-08 08:14:38 -0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import SignupForm from "./components/SignupForm";
+import LoginForm from "./components/LoginForm";
 import "./App.css";
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path='/' element={<Home />} />
+        <Route exact path='/' element={<Home />} />
         <Route path='/signup' element={<SignupForm />} />
+        <Route path='/login' element={<LoginForm />} />
       </Routes>
     </Router>
   );
