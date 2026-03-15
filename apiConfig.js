ef00761fef9ffdb51228f22125ec987a3d795604
shruti03-design <shrutisen03@gmail.com>
2026-03-15 13:27:56 -0700

    can search with tags

--- apiConfig.js
+++ apiConfig.js
@@ -41,6 +41,7 @@ export const BUSINESS_INFO_ENDPOINT = `${API_BASE_URL}/api/v1/businessinfo`;
 export const BUSINESSES_ENDPOINT = `${API_BASE_URL}/businesses`;
 export const CATEGORY_LIST_ENDPOINT = `${API_BASE_URL}/category_list/all`;
 export const BUSINESS_AVG_RATINGS_ENDPOINT = `${API_BASE_URL}/api/v1/businessavgratings`;
+export const BUSINESS_TAG_SEARCH_ENDPOINT = `${API_BASE_URL}/api/v1/businesstagsearch`;
 
 // Search and Network Endpoints
 // export const BUSINESS_RESULTS_ENDPOINT = `${API_BASE_URL}/api/businessresults`;
