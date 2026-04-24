f3b6083de3ec8aa9b88450dd21abb5d1d6ee5e52
pmarathay <pmarathay@infiniteoptions.com>
2026-04-24 14:17:44 -0700

    updated version number

--- apiConfig.js
+++ apiConfig.js
@@ -2,7 +2,7 @@
 // Uncomment the appropriate line for your environment
 
 // Production/Development Environment
-//export const API_BASE_URL = "https://o7t5ikn907.execute-api.us-west-1.amazonaws.com/dev";
+export const API_BASE_URL = "https://o7t5ikn907.execute-api.us-west-1.amazonaws.com/dev";
 // export const SEARCH_BASE_URL = "http://54.183.12.163:5001";
 // export const SEARCH_BASE_URL = "http://13.52.244.236:5001";
 // export const SEARCH_BASE_URL = "http://13.52.82.86:5001";
@@ -12,7 +12,7 @@ export const SEARCH_BASE_URL = "https://ioec2vrecsearch.infiniteoptions.com";
 // export const API_BASE_URL = "https://ioec2ecaspm.infiniteoptions.com";
 
 // Local Development (commented out by default)
- export const API_BASE_URL = "http://localhost:4090";
+//  export const API_BASE_URL = "http://localhost:4090";
 // export const API_BASE_URL = "http://127.0.0.1:4090";
 
 // Google and Apple Authentication Endpoints
